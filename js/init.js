a11f1f80de65e4d0bc95c63f348fbb55c6067db4
jtx007 <jamesjacobthomas7@gmail.com>
2018-07-19 17:03:49 -0400

    Done.

--- js/init.js
+++ js/init.js
@@ -1,12 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var API_KEY = "Go get an API key";
+  var API_KEY = "221948f2de097d880248f8277c7ef0ae";
   var CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
-  var URL = CORS_WRAPPER + "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
+  var URL = CORS_WRAPPER + "https://api.darksky.net/forecast/"+ API_KEY + "/40.7127,-74.0059?exclude=currently?exclude=minutely?exclude=daily"
+
   var ctx = document.getElementById("NYCWeatherChart").getContext("2d")
 
 
-  makeRequest(URL, function(json) {
-    var data = generateDataSet(getHours(json), getFahrenheits(json));
-    var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
-  });
+  makeRequest(URL, function(data) {
+    console.log(data)
+    for(let i = 0; i < data['hourly']['data'].length; i++) {
+
+
+    console.log(data['hourly']['data'][i]['temperature'])
+    console.log(data['hourly']['data'][i]['time'])
+   }
+ })
+  // fetch(URL)
+  // .then(function(response) {
+  //   return response.json();
+  // })
+  // .then(function(myJSON) {
+  //   console.log(myJSON);
+  // });
+  // makeRequest(URL, function(json) {
+  //   var data = generateDataSet(getHours(json), getFahrenheits(json));
+  //   var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
+  // });
 });
